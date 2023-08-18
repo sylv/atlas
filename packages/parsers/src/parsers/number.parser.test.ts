00ff4b59d66fd0308efaccb3ef0f550fa60e8bf2
ryan <ryan@sylver.me>
2023-08-18 18:02:08 +0800

    fix number parser with "k" suffixes

--- packages/parsers/src/parsers/number.parser.test.ts
+++ packages/parsers/src/parsers/number.parser.test.ts
@@ -1,13 +1,21 @@
-import { expect, test } from 'vitest';
+import { expect, it } from 'vitest';
 import { parseNumber } from './number.parser.js';
 
-test('it should parse numbers', () => {
+it('it should parse numbers', () => {
   expect(parseNumber('1 thousand')).toBe(1000);
   expect(parseNumber('one thousand')).toBe(1000);
   expect(parseNumber('$50k')).toBe(50_000);
+  expect(parseNumber('$50 k')).toBe(50_000);
+  expect(parseNumber('50k')).toBe(50_000);
   expect(parseNumber('twenty-two thousand')).toBe(22_000);
   expect(parseNumber('tonight at eight pm')).toBe(8);
   expect(parseNumber('22 hours 15 minutes')).toBe(22);
   expect(parseNumber('10,100.20')).toBe(10_100.2);
   expect(parseNumber('10,10')).toBeNull();
+  expect(parseNumber('2,000')).toBe(2000);
+});
+
+it('should not parse snowflakes as numbers', () => {
+  expect(parseNumber('Hello <@111372124383428608>')).toBeNull();
+  expect(parseNumber('111372124383428608')).toBeNull();
 });
--- packages/parsers/src/parsers/number.parser.ts
+++ packages/parsers/src/parsers/number.parser.ts
@@ -2,11 +2,47 @@
 import { NUMBER_REGEX } from '@atlasbot/core';
 import nlp from 'compromise';
 
+const CUSTOM_UNITS = new Map([
+  ['k', 1000],
+  ['m', 1e6],
+  ['b', 1e9],
+]);
+
+interface JSON {
+  text: string;
+  number: { prefix: string; num: number; suffix: string; hasComma: boolean; unit: string };
+}
+
+// i basically pulled this out of my ass
+const MAX_SAFE_NUMBER = 100_000_000_000;
+const COMMAN_MATCH_REGEX = /^\d{1,3}(,\d{3})+(\.\d+)?$/; // disallow "10,10", only allow "10,000", "10,000.5" etc
+
 export const parseNumber = (input: string): number | null => {
-  // regex is faster than compromise and will match simple values
-  if (NUMBER_REGEX.test(input)) return Number(input);
+  if (NUMBER_REGEX.test(input)) {
+    // regex is faster than compromise and will match simple values
+    const result = Number(input);
+    if (result > MAX_SAFE_NUMBER) return null;
+    return result;
+  }
+
   const document = nlp(input);
-  const value = document.numbers().get(0);
-  if (Array.isArray(value)) return value[0];
-  return value;
+  const values = document.numbers().toNumber().json() as JSON[];
+  for (const value of values) {
+    // disallow "10,10" which compromise parses to "1010"
+    if (value.number.hasComma && !COMMAN_MATCH_REGEX.test(value.text)) {
+      continue;
+    }
+
+    // map "10k" to "10000" and not "10"
+    // there is surely a better way to do this but compromise docs are a... compromise :)
+    // and i really don't want to spend any more of my time on this.
+    const customUnit = CUSTOM_UNITS.get(value.number.suffix) || CUSTOM_UNITS.get(value.number.unit);
+    const realValue = customUnit ? value.number.num * customUnit : value.number.num;
+    if (realValue > MAX_SAFE_NUMBER) continue;
+    return realValue;
+  }
+
+  return null;
 };
+
+console.log(parseNumber('111372124383428608'));
